f5d654cf51588d8294e199a6cb3b8e428587ed98
David Stewart <davidstewy@gmail.com>
2018-07-19 21:37:23 -0400

    unbeautify

--- app.js
+++ app.js
@@ -8,37 +8,33 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'myproject';
 
 //Use connect method to connect to the server
-MongoClient.connect(url, function (err, client) {
+MongoClient.connect(url, function(err, client){
    assert.equal(null, err);
    console.log("Connected successfully to server");
 
    const db = client.db(dbName);
 
-   insertDocuments(db, function () {
+   insertDocuments(db, function() {
       // findDocuments(db, function(){})
-      indexCollection(db, function () {
-         // updateDocument(db, function() {
-         //    removeDocument(db, function() {
+      indexCollection(db, function() {
+      // updateDocument(db, function() {
+      //    removeDocument(db, function() {
 
-         client.close();
-      });
-   })
-});
-
-
+            client.close();
+         });
+      })
+   });
 
 
-const insertDocuments = function (db, callback) {
+
+
+const insertDocuments = function(db, callback) {
    // Get the documents collection
    const collection = db.collection('documents');
    // Insert some documents
-   collection.insertMany([{
-      a: 1
-   }, {
-      a: 2
-   }, {
-      a: 3
-   }], function (err, result) {
+   collection.insertMany([
+      {a : 1}, {a : 2}, {a:3}
+   ], function(err, result){
       assert.equal(err, null);
       assert.equal(3, result.result.n);
       assert.equal(3, result.ops.length);
@@ -47,13 +43,11 @@ const insertDocuments = function (db, callback) {
    });
 }
 
-const findDocuments = function (db, callback) {
+const findDocuments = function(db, callback) {
    //Get the documents collection
    const collection = db.collection('documents');
    // Find some documents
-   collection.find({
-      'a': 3
-   }).toArray(function (err, docs) {
+   collection.find({'a': 3}).toArray(function(err, docs) {
       assert.equal(err, null);
       console.log("Found the following records");
       console.log(docs)
@@ -61,17 +55,12 @@ const findDocuments = function (db, callback) {
    });
 }
 
-const updateDocument = function (db, callback) {
+const updateDocument = function(db, callback) {
    // Get the documents collection
    const collection = db.collection('documents');
    // Update document where a is 2, set b equal to 1
-   collection.updateOne({
-      a: 2
-   }, {
-      $set: {
-         b: 1
-      }
-   }, function (err, result) {
+   collection.updateOne({ a : 2 }
+   , { $set: { b : 1 } }, function(err, result) {
       assert.equal(err, null);
       assert.equal(1, result.result.n);
       console.log("Updated the document with the field a equal to 2");
@@ -79,13 +68,11 @@ const updateDocument = function (db, callback) {
    });
 }
 
-const removeDocument = function (db, callback) {
+const removeDocument = function(db, callback) {
    // Get the documents collection
    const collection = db.collection('documents');
    // Delete document where a is 3
-   collection.deleteOne({
-      a: 3
-   }, function (err, result) {
+   collection.deleteOne({ a : 3 }, function(err, result) {
       assert.equal(err, null);
       assert.equal(1, result.result.n);
       console.log("Removed the document with the field a equal to 3");
@@ -93,12 +80,11 @@ const removeDocument = function (db, callback) {
    });
 }
 
-const indexCollection = function (db, callback) {
-   db.collection('documents').createIndex({
-         "a": 1
-      },
+const indexCollection = function(db, callback) {
+   db.collection('documents').createIndex(
+      { "a": 1},
       null,
-      function (err, results) {
+      function(err, results) {
          console.log(results);
          callback();
       }
