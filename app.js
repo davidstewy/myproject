cdb7cd93edaa00a1ff3e6f0ddefdcd27c8d7c8b6
David Stewart <davidstewy@gmail.com>
2018-07-20 11:49:29 -0400

    took out comments

--- app.js
+++ app.js
@@ -15,11 +15,8 @@ MongoClient.connect(url, function(err, client){
    const db = client.db(dbName);
 
    insertDocuments(db, function() {
-      // findDocuments(db, function(){})
       indexCollection(db, function() {
-      // updateDocument(db, function() {
-      //    removeDocument(db, function() {
-
+     
             client.close();
          });
       })
@@ -33,7 +30,7 @@ const insertDocuments = function(db, callback) {
    const collection = db.collection('documents');
    // Insert some documents
    collection.insertMany([
-      {a : 1}, {a : 2}, {a:3}
+      {a : 1}, {a : 2}, {a : 3}
    ], function(err, result){
       assert.equal(err, null);
       assert.equal(3, result.result.n);
